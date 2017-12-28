4de3a4cb34028a2c405327acc1d75cf12f35d6a4
vikash-ag <vikashagarwal208.va@gmail.com>
2017-12-28 15:25:37 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require('express');
+	app = express();
+	
+	
+app.set('port' , process.env.PORT || 5000);
+
+app.get('/' , function(req,res){ 
+	res.send('Concentrate on your project!!!');
+	console.log('this the console example...');	
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port -> ' + app.get('port'));
+});
